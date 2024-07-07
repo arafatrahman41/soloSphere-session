56fe3d5112c421bc777b48da9b732735b76a7979
arafatrahman41 <arafatrahman41@outlook.com>
2024-07-07 09:42:06 +0600

    starting the soloSphere project of conceptual session

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+        <h2 className="text-3xl font-bold">this is home</h2>    
+        <button className="btn btn-circle"> btn</button>         
+        </div>
+    );
+};
+
+export default Home;
